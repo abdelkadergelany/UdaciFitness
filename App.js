e6c74538c12d1bc8e43844c423903be529b23cbf
aly-abdelkader-iut-cse-4301 <38514887+aly-abdelkader-iut-cse-4301@users.noreply.github.com>
2020-11-29 08:51:30 +0600

    finsishing navigation

--- App.js
+++ App.js
@@ -1,19 +1,112 @@
-import React from 'react'
-import { View } from 'react-native'
+import 'react-native-gesture-handler'
+import React, { Component } from 'react'
+import { View, StatusBar, Platform } from "react-native"
+import { NavigationContainer, TabActions } from "@react-navigation/native"
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs"
+import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs'
+import { createStackNavigator } from "@react-navigation/stack";
 import AddEntry from './components/AddEntry'
 import { createStore } from 'redux'
 import { Provider } from 'react-redux'
 import reducer from './reducers'
 import History from './components/History'
+import { Ionicons, FontAwesome } from "@expo/vector-icons";
+import { orange, white } from "./utils/colors";
+import Constants from "expo-constants";
+import EntryDetail from './components/EntryDetail'
+import Live from "./components/Live"
+import { setLocalNotification } from "./utils/helpers"
 
-export default class App extends React.Component {
-  render() {
-    return (
-      <Provider store={createStore(reducer)}>
-        <View style={{flex: 1}}>
-          <History />
-        </View>
-      </Provider>
-    )
-  }
+
+
+function UdaciStatusBar({ backgroundColor, ...props }) {
+  return (
+    <View style={{ backgroundColor, height: Constants.statusBarHeight }}>
+      <StatusBar translucent backgroundColor={backgroundColor} {...props} />
+    </View>
+  )
+}
+
+const Tabs = Platform.OS === "ios" ? createBottomTabNavigator() : createMaterialTopTabNavigator();
+
+const TabNav = () => (
+  <Tabs.Navigator
+    initialRouteName="AddEntry"
+    screenOptions={({ route }) => ({
+      tabBarIcon: ({ color, size }) => {
+        let icon;
+        if (route.name === "Add Entry") {
+          icon = <FontAwesome name="plus-square" size={size} color={color} />;
+        } else if (route.name === "History") {
+          icon = <Ionicons name="ios-bookmarks" size={size} color={color} />;
+        } else if (route.name === "Live") {
+          icon = <Ionicons name="ios-speedometer" size={size} color={color} />;
+        }
+        return icon;
+      },
+    })}
+    tabBarOptions={{
+      header: null,
+      activeTintColor: Platform.OS === "ios" ? orange : white,
+      showIcon: true,
+      style: {
+        height: 80,
+        backgroundColor: Platform.OS === "ios" ? white : orange,
+        shadowColor: "rgba(0, 0, 0, 0.24)",
+        shadowOffset: {
+          width: 0,
+          height: 3,
+        },
+        shadowRadius: 6,
+        shadowOpacity: 1,
+      },
+    }}
+  >
+    <Tabs.Screen name="Add Entry" component={AddEntry} />
+    <Tabs.Screen name="History" component={History} />
+    <Tabs.Screen name="Live" component={Live} />
+  </Tabs.Navigator>
+);
+
+const Stack = createStackNavigator();
+
+const MainNav = () => (
+  <Stack.Navigator headerMode="screen">
+    <Stack.Screen
+      name="Home"
+      component={TabNav}
+      options={{ headerShown: false }}
+    />
+    <Stack.Screen
+      name="EntryDetail"
+      component={EntryDetail}
+      options={{
+        headerTintColor: white,
+        headerStyle: {
+          backgroundColor: orange,
+        },
+      }}
+    />
+  </Stack.Navigator>
+);
+
+
+export default function App() {
+  React.useEffect(() => {
+    //setLocalNotification();
+  });
+  React.useEffect(() => {
+   // setLocalNotification()
+  })
+  return (
+    <Provider store={createStore(reducer)}>
+      <View style={{ flex: 1 }}>
+        <NavigationContainer>
+          <UdaciStatusBar backgroundColor={orange} barStyle="light-content" />
+          <MainNav />
+        </NavigationContainer>
+      </View>
+    </Provider>
+  )
+
 }
---
+++ components/EntryDetail.js
@@ -0,0 +1,84 @@
+import React, { Component } from "react";
+import { View, Text, StyleSheet } from "react-native";
+import { connect } from "react-redux";
+import { white } from "../utils/colors";
+import MetricCard from "../components/MetricCard";
+import { addEntry } from "../actions";
+import { removeEntry } from "../utils/api";
+import { timeToString, getDailyReminderValue } from "../utils/helpers";
+import AddEntry from "./AddEntry";
+import TextButton from "../components/TextButton";
+import { CommonActions } from "@react-navigation/native";
+
+class EntryDetail extends Component {
+  setTitle = (entryId) => {
+    if (!entryId) return;
+
+    const year = entryId.slice(0, 4);
+    const month = entryId.slice(5, 7);
+    const day = entryId.slice(8);
+
+    this.props.navigation.setOptions({
+      title: `${month}/${day}/${year}`,
+    });
+  };
+
+  reset = () => {
+    const { remove, goBack, entryId } = this.props;
+
+    remove();
+    goBack();
+    removeEntry(entryId);
+  };
+
+  shouldComponentUpdate(nextProps) {
+    return nextProps.metrics.length !== 0 && !nextProps.metrics[0].today;
+  }
+
+
+  render() {
+    const { entryId } = this.props.route.params;
+    this.setTitle(entryId);
+    return (
+      <View style={styles.container}>
+        <MetricCard metrics={this.props.metrics[0]} />
+        <TextButton onPress={this.reset} style={{ margin: 20 }}>
+          RESET
+        </TextButton>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: white,
+    padding: 15,
+  },
+});
+
+function mapStateToProps(state, { route }) {
+  const { entryId } = route.params;
+  return {
+    entryId,
+    metrics: state[entryId],
+  };
+}
+
+function mapDispatchToProps(dispatch, { navigation, route }) {
+  const { entryId } = route.params;
+
+  return {
+    remove: () =>
+      dispatch(
+        addEntry({
+          [entryId]:
+            entryId === timeToString() ? getDailyReminderValue() : new Array(),
+        })
+      ),
+      goBack: () => navigation.dispatch(CommonActions.goBack()),
+  };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(EntryDetail);
